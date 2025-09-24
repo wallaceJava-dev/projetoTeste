d9a1d925bb350c1f180033cbc66e96fe5014efc1
wallaceJava-dev <wallace.devjava@gmail.com>
2025-09-24 19:45:52 -0300

    Adiciona entidade Usuario e modifica Application

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.dominio.entidade.Usuario;
+import org.apache.catalina.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
---
+++ src/main/java/com/example/demo/dominio/entidade/Usuario.java
@@ -0,0 +1,24 @@
+package com.example.demo.dominio.entidade;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+    private String senha;
+}
